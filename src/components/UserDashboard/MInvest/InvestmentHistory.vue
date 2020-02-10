7997b88a0d989e4cbdac82082134c283a9c80c9e
Mustapha Abdulmujeeb <mostalpha7@gmail.com>
2020-02-10 06:23:22 -0800

    User & Admin dashboard update 10/02/2020

--- src/components/UserDashboard/MInvest/InvestmentHistory.vue
+++ src/components/UserDashboard/MInvest/InvestmentHistory.vue
@@ -1,5 +1,9 @@
 <template>
-  <Structure page="Investment History"></Structure>
+  <Structure page="Investment History">
+    <div
+      class="container"
+    >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor obcaecati harum quisquam culpa ea suscipit eaque facilis nihil tempore, corporis architecto ex ut aliquam doloribus aspernatur eius? Velit, saepe culpa.</div>
+  </Structure>
 </template>
 <script>
 import Structure from "../GUserLayouts/Structure";
